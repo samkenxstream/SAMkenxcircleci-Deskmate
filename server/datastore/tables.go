cc755d92559dbfdf794818d02bd4adfbd7c139a2
Tyler Conlee <tylerlconlee@gmail.com>
2022-06-01 09:48:29 -0700

    add not null constrains to db table creation

--- server/datastore/tables.go
+++ server/datastore/tables.go
@@ -16,13 +16,13 @@ func checkTable() {
 func createConfigTable() {
 	const query = `
 	CREATE TABLE IF NOT EXISTS configuration (
-		id serial PRIMARY KEY,
-		slack_api text,
-		slack_url text,
-		slack_signing text,
-		zendesk_url text,
-		zendesk_user text,
-		zendesk_api text
+		id serial PRIMARY KEY NOT NULL,
+		slack_api text NOT NULL,
+		slack_url text NOT NULL,
+		slack_signing text NOT NULL,
+		zendesk_url text NOT NULL,
+		zendesk_user text NOT NULL,
+		zendesk_api text NOT NULL
 	)`
 	// Exec executes a query without returning any rows.
 	result, err := db.Exec(query)
@@ -45,11 +45,11 @@ func createConfigTable() {
 func createTriageTable() {
 	const query = `
 	CREATE TABLE IF NOT EXISTS triage (
-		id serial PRIMARY KEY,
-		name text,
-		slack_id text,
-		channel text,
-		started timestamp
+		id serial PRIMARY KEY NOT NULL,
+		name text NOT NULL,
+		slack_id text NOT NULL,
+		channel text NOT NULL,
+		started timestamp NOT NULL
 	)`
 	// Exec executes a query without returning any rows.
 	result, err := db.Exec(query)
@@ -72,13 +72,13 @@ func createTriageTable() {
 func createTagsTable() {
 	const query = `
 	CREATE TABLE IF NOT EXISTS tags (
-		id serial PRIMARY KEY,
-		tag text,
-		slack_id text,
-		group_id text,
-		channel text,
-		notification_type text,
-		added timestamp
+		id serial PRIMARY KEY NOT NULL,
+		tag text NOT NULL,
+		slack_id text NOT NULL,
+		group_id text NOT NULL,
+		channel text NOT NULL,
+		notification_type text NOT NULL,
+		added timestamp NOT NULL
 	)`
 	// Exec executes a query without returning any rows.
 	result, err := db.Exec(query)
